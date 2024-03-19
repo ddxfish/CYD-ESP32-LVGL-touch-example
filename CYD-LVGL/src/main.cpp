4bf6dcb8ddf038feedc904f6e00868ffd01b57f4
ddxfish <ddxfish@gmail.com>
2024-03-19 11:26:04 -0400

    first working demo

--- CYD-LVGL/src/main.cpp
+++ CYD-LVGL/src/main.cpp
@@ -6,13 +6,23 @@
 #include "SPI.h"
 #include "XPT2046_Bitbang.h"
 
+//TF Card
 #define SD_CS 5 // Adjust to your SD card CS pin
 
+//TFT_eSPI used as LVGL display driver
 TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
-static lv_disp_buf_t disp_buf;
-static lv_color_t buf[LV_HOR_RES_MAX * 10]; /* Declare a buffer for 10 lines */
 
+//LVGL (not all of this has to be global, but it's easier for now)
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[LV_HOR_RES_MAX * 10]; // /* Declare a buffer for 10 lines */
+static lv_disp_drv_t disp_drv;  // Display driver
+lv_obj_t *atextlabel = nullptr;
 
+//Our globals. dont use global variables like this. stop it, get some help.
+int counter = 0;
+String printthis = "";
+
+//Touchscreen
 #define MOSI_PIN 32
 #define MISO_PIN 39
 #define CLK_PIN  25
@@ -20,172 +30,217 @@ static lv_color_t buf[LV_HOR_RES_MAX * 10]; /* Declare a buffer for 10 lines */
 #define RERUN_CALIBRATE true
 XPT2046_Bitbang touchscreen(MOSI_PIN, MISO_PIN, CLK_PIN, CS_PIN);
 
-    /* Display flushing */
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+// Flush display - this is the link between LVGL and TFT_eSPI for LVGL v8
+void cyd_disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
     Serial.println("Flushing display...");
-
     uint32_t w = (area->x2 - area->x1 + 1);
     uint32_t h = (area->y2 - area->y1 + 1);
-
     tft.startWrite();
     tft.setAddrWindow(area->x1, area->y1, w, h);
 
-    // Directly send pixel data to the display
-    for(int y = area->y1; y <= area->y2; y++) {
-        for(int x = area->x1; x <= area->x2; x++) {
-            // Calculate the position within the buffer
-            uint32_t buffer_pos = (y - area->y1) * w + (x - area->x1);
-            // Extract the color from LVGL's buffer and write to display
-            uint16_t color = color_p[buffer_pos].full;
-            tft.writeColor(color, 1);
-        }
-    }
-
+    // Use the pushColors() method to write the pixel data to the display.
+    tft.pushColors((uint16_t*)color_p, w * h, true);
     tft.endWrite();
-    lv_disp_flush_ready(disp); // Inform LVGL that flushing is done
 
+    lv_disp_flush_ready(disp_drv); // Inform LVGL that flushing is done
     Serial.println("Display flushed.");
-
-
 }
 
-void setup() {
-    Serial.begin(115200);
-    while(!Serial); // Wait for serial port to connect. Needed for native USB
-    Serial.println("Starting setup...");
 
-    pinMode(21, OUTPUT); // Set pin 21 as an output for the backlight
-    digitalWrite(21, HIGH); // Turn on the backlight
-
+void initTouchscreen(){
     // Initialize the touchscreen
     touchscreen.begin();
     // Check for existing calibration data
     if (!touchscreen.loadCalibration()) {
         Serial.println("Failed to load calibration data from SPIFFS.");
     }
-    // Check if we need to re-run calibration
-    #if RERUN_CALIBRATE
-        Serial.println("Re-running calibration as requested...");
-        delay(2000); //wait for user to see serial
-        touchscreen.calibrate();
-        touchscreen.saveCalibration();
-    #endif
+}
 
-    delay(1000);
-    Serial.println("Backlight enabled.");
+void initSPIFFS(){
+    // Initialize SPIFFS for calibration data
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An error has occurred while mounting SPIFFS");
+        return;
+    }
+}
 
+void calibrateTouchscreen(){
+    Serial.println("Re-running calibration as requested...");
+    delay(2000); //wait for user to see serial
+    touchscreen.calibrate();
+    touchscreen.saveCalibration();
+}
+
+void enableTouchscreen(){
+    // Initialize SPIFFS for calibration data
+    initSPIFFS();
+
+    // Initialize the touchscreen
+    touchscreen.begin();
+
+    // Check for existing calibration data
+    if ((!touchscreen.loadCalibration()) || (RERUN_CALIBRATE)) {
+        Serial.println("Re-calibrating. Either RERUN_CALIBRATE is set or no calibration data found.");
+        calibrateTouchscreen();
+    }
+}
+
+void turnOnBacklight(){
+    pinMode(21, OUTPUT); // Set pin 21 as an output for the backlight
+    digitalWrite(21, HIGH); // Turn on the backlight
+}
+
+void configureLVGL(){    
+    //LVGL - graphics library that interfaces with TFT_eSPI
+    Serial.println("LVGL initializing.");
     lv_init();
-    Serial.println("LVGL initialized.");
 
-    tft.begin(); /* TFT init */
-    Serial.println("TFT initialized.");
-    tft.setRotation(1); /* Landscape orientation */
-    Serial.println("TFT rotation set.");
+     //LVGL configuration
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LV_HOR_RES_MAX * 10);
+    lv_disp_drv_init(&disp_drv); // Basic initialization
+    disp_drv.draw_buf = &draw_buf; // Assign the initialized buffer
+    disp_drv.flush_cb = cyd_disp_flush; // Set your display's flush callback
+    disp_drv.hor_res = 240;  // Set the horizontal resolution of your display
+    disp_drv.ver_res = 320;  // Set the vertical resolution of your display
+    lv_disp_drv_register(&disp_drv); // Register the driver
 
-    lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = 240;
-    disp_drv.ver_res = 320;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.buffer = &disp_buf;
-    lv_disp_drv_register(&disp_drv);
-    Serial.println("LVGL display driver registered.");
+}
 
-    // SD Card Initialization
+void SDCardInit(){
+    //SD Card
     if(!SD.begin(SD_CS)) {
         Serial.println("Card Mount Failed");
         return;
     }
-    Serial.println("SD card initialized.");
-
-    // Format SD card
-    if(SD.exists("/hello.txt")) {
-        SD.remove("/hello.txt");
-        Serial.println("/hello.txt exists. Removing...");
+    uint8_t cardType = SD.cardType();
+    if(cardType == CARD_NONE) {
+        Serial.println("No SD card attached");
+        return;
     }
+    Serial.println("SD card initialized.");
+}
 
-    // Create a new file
+void removeSDCardFile(const char* path){
+    if(SD.exists(path)) {
+        SD.remove(path);
+        Serial.println("Removed existing file: " + String(path));
+    }
+}
+
+void createNewSDCardFile(const char* path){
+    File file = SD.open(path, FILE_WRITE);
+    if(!file) {
+        Serial.println("Failed to create file");
+        return;
+    }
+    file.close();
+    Serial.println("Created new file: " + String(path));
+}
+
+void writeToSDCardFile(String data){
+    Serial.println("Written 'Hello, hi from SD card!' to /hello.txt");
     File file = SD.open("/hello.txt", FILE_WRITE);
     if(!file) {
         Serial.println("Failed to open file for writing");
         return;
     }
-    file.println("Hello, hi from SD card!");
+    file.println("Hello, fellow CYD user!");
     file.close();
-    Serial.println("Written 'Hello, hi from SD card!' to /hello.txt");
+}
 
-    // Read the file
-    file = SD.open("/hello.txt");
+String readLineFromSDCardFile(const char* path){
+    File file = SD.open(path);
+    String thereadline;
     if(!file) {
         Serial.println("Failed to open file for reading");
-        return;
+        return thereadline;
     }
-    String line;
-    while(file.available()){
-        line = file.readStringUntil('\n');
-        break; // Just read the first line for simplicity
+    Serial.println("Reading from file: " + String(path));
+    while(file.available()) {
+        thereadline = file.readStringUntil('\n');
+        Serial.println(thereadline);
     }
     file.close();
-    Serial.println("Read from /hello.txt: " + line);
+    Serial.println("First line read from file: " + thereadline);
+    return thereadline;
+}
 
-
-    // Create a style for the text
-    static lv_style_t style_text;
-    lv_style_init(&style_text);
-    // Set the text color. Example: RGB(255, 0, 0) for red
-    lv_style_set_text_color(&style_text, LV_STATE_DEFAULT, LV_COLOR_MAKE(255, 0, 0));
-
-
-    // Display the text from SD card
-    lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);
-    
-    lv_label_set_text(label, line.c_str());
-    lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
+void pushTextToScreen(String text) {
+    if (!atextlabel) {
+        atextlabel = lv_label_create(lv_scr_act());
+        lv_obj_align(atextlabel, LV_ALIGN_CENTER, 0, 0);
+    }
+    Serial.println("Setting text on label object.");
+    lv_label_set_text(atextlabel, text.c_str());
     Serial.println("Displayed text on screen.");
 }
-int counter = 0;
-unsigned long lastTick = 0;
-void loop() {
 
 
-    static lv_obj_t* circle = nullptr; // Define static to retain the value across loop() calls
-    Point touchPoint = touchscreen.getTouch(); // Poll the touch controller
+void setup() {
+    Serial.begin(115200);
+    while(!Serial); // Wait for serial port to connect
+    delay(2000);
 
-    if (touchPoint.x != 0) { // Valid touch detected
-        Serial.println("cirle1");
-        //if(circle == nullptr) {
-            Serial.println("cirle2");
-            // Create the circle on the first valid touch
-            circle = lv_obj_create(lv_scr_act(), NULL);
-            lv_obj_set_size(circle, 200, 200); // Adjust size for better visibility
-            lv_obj_set_style_local_bg_color(circle, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_BLUE); // Use a contrasting color
-        //}
-        
-        // Move the circle to the touch location on every valid touch
-        lv_obj_set_pos(circle, touchPoint.x - 10, touchPoint.y - 10); // Adjust the offset as needed
-    }
+    //Backlight
+    Serial.println("Turning on backlight...");
+    turnOnBacklight();
+
+    //Touchscreen XPT2046 Bitbang
+    Serial.println("Enabling touchscreen...");
+    enableTouchscreen();
+
+    //TFT_eSPI as display driver
+    Serial.println("TFT init and rotation.");
+    tft.begin(); /* TFT init */
+    tft.setRotation(0); /* Landscape orientation */
+    tft.fillScreen(TFT_BLACK); // Clear the screen to black
+    // Draw a red circle
+    tft.drawCircle(120, 160, 50, TFT_RED); // Parameters: x, y, radius, color
+    delay(2000);
+
+    //Config LVGL
+    Serial.println("Configuring LVGL...");
+    configureLVGL();
+
+    //SDCard
+    Serial.println("SD card initization.");
+    SDCardInit();
+    // Remove existing file if any
+    Serial.println("Removing existing file /hello.txt if any.");
+    removeSDCardFile("/hello.txt");
+    // Create a new file
+    createNewSDCardFile("/hello.txt");
+    // Write to the file
+    writeToSDCardFile("Hello, fellow CYD user!");
+    // Read from the file
+    printthis = readLineFromSDCardFile("/hello.txt");
+    Serial.println("Line read from file: " + printthis);
+
+    // Push some text to the screen as a test
+    Serial.println("Pushing text to screen.");
+    pushTextToScreen("Hello, CYD user!");
+
+}
 
 
-    Serial.println("touchscreen location: " + String(touchPoint.x) + ", " + String(touchPoint.y));
-        // Read the file
-    File file = SD.open("/hello.txt");
-    if(!file) {
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-    String line;
-    while(file.available()){
-        line = file.readStringUntil('\n');
-        break; // Just read the first line for simplicity
-    }
-    file.close();
-    Serial.println("Read from /hello.txt: " + line + " " + String(counter));
-
+void loop() { 
+    Serial.println("Looping..." + String(counter));
     
+    //printthis = "none";
+    //printthis = readLineFromSDCardFile("/hello.txt"); //this reads from SD, high wear for a loop
+    //Serial.println("Line read from file: " + printthis);
+    Serial.println("Pushing text to screen.");
+    pushTextToScreen("Hello, CYD user!");
 
+    Point touchPoint = touchscreen.getTouch(); // Poll the touch controller
+    if (touchPoint.x != 0) { // No touch should return 0,0
+        Serial.println("touchscreen location: " + String(touchPoint.x) + ", " + String(touchPoint.y));
+    }
 
     lv_task_handler(); /* Handle LVGL tasks */
-    delay(10);
-    counter +=1;
+
+    Serial.print("Available heap memory: " + String(esp_get_free_heap_size()) + " bytes");
+
+    delay(500);
+    counter += 1;
 }
